bade86de611198752e67bdbac8372915aa390e3c
Armili kadakuntla <armilikadakuntla@gmail.com>
2026-07-03 13:41:44 +0530

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app=express();
-const PORT=3002;
+const PORT=process.env.port||3002;
 app.use(express.static("frontend"))
 //web server:
 app.listen(PORT,function(){
