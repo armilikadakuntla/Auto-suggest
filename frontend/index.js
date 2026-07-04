83ebf8a5642d5633b1eaa6f26b9e0224988c46a2
Armili kadakuntla <armilikadakuntla@gmail.com>
2026-07-04 09:05:34 +0530

    Added toggle feature

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,26 @@
-console.log("hello from js");
-alert("hello from js");
+
+const users = [
+  {
+    "name" :"Akshitha",
+    "gender" : "Female",
+    "image" : "./images/avatar.jpg"
+  },
+  {
+    "name" :"Armili",
+    "gender" : "Female",
+    "image" : "./images/Armili.jpg"
+  },
+]
+let curUserId = 0;
+function toggle(){
+  if(curUserId == 0)
+    curUserId=1;
+  else
+    curUserId=0;
+   document.getElementById("profile-img").src = users[curUserId].image;
+    document.getElementById("card-name").innerText = users[curUserId].name;
+    document.getElementById("card-gender").innerText = users[curUserId].gender;
+}
+
+  console.log("Hello from JS");
+  alert("hello from js");
